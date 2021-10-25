601a5194888b69ce1c8c405e5f964b858978c3e7
jairui <jairuigou@gmail.com>
2021-10-25 13:11:14 +0800

    Add toggle to priorlabel

--- src/components/PriorLabel.vue
+++ src/components/PriorLabel.vue
@@ -1,16 +1,22 @@
 <template>
-  <Label :labelText="labelText" :backgroundColor="backgroundColor"></Label>
+  <Label v-on:click="toggle" :labelText="labelText" :backgroundColor="backgroundColor"></Label>
+  <span ref="toggle" class="hidden">
+      <button ref="upgrade" v-on:click="upgrade">up</button>
+      <button ref="downgrade" v-on:click="downgrade">down</button>
+  </span>
 </template>
 
 <script>
 import Label from "./LabelBase.vue"
 
 export default{
+    data(){
+        return{
+            labelText: "LOW"
+        }
+    },
     components:{
         Label
-    },
-    props:{
-        labelText: String
     },
     computed:{
         backgroundColor:function(){
@@ -25,10 +31,45 @@ export default{
             }
             return "black";
         }
+    },
+    methods:{
+        toggle: function(event){
+          var toggle = this.$refs.toggle;
+          toggle.classList.toggle("hidden");
+          if(this.labelText == "LOW"){
+              this.$refs.downgrade.disabled = true;
+          }
+          else if( this.labelText == "HIGH"){
+              this.$refs.upgrade.disabled = true;
+          }
+        },
+        upgrade: function(event){
+            if( this.labelText == "LOW" ){
+                this.labelText = "MEDIUM";
+                this.$refs.downgrade.disabled = false;
+            }
+            else if( this.labelText == "MEDIUM" ){
+                this.labelText = "HIGH";
+                this.$refs.upgrade.disabled = true;
+            }
+        },
+        downgrade: function(event){
+            if( this.labelText == "HIGH" ){
+                this.labelText = "MEDIUM";
+                this.$refs.upgrade.disabled = false;
+            }
+            else if( this.labelText == "MEDIUM" ){
+                this.labelText = "LOW";
+                this.$refs.downgrade.disabled = true;
+            }
+        } 
     }
 
 }
 </script>
 
 <style scoped>
+.hidden{
+    display: none;
+}
 </style>
